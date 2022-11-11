133c327450befaf0ff70338fc145ef7dde3613e2
matthewngc <13530@coderacademy.edu.au>
2022-11-12 01:07:53 +1100

    Added User model and schema

---
+++ src/models/user.py
@@ -0,0 +1,16 @@
+from init import db, ma
+from marshmallow import fields
+
+class User(db.model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    date_joined = db.Column(db.Date)
+    is_admin = db.Column(db.Boolean, default = False)
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'email', 'password', 'date_joined', 'is_admin')
