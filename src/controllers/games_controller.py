ee78c1b9c67903c2ca4d0ab70de868d9bf83cd72
matthewngc <13530@coderacademy.edu.au>
2022-11-11 21:23:26 +1100

    Added GET and POST game controllers

--- src/controllers/games_controller.py
+++ src/controllers/games_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from init import db
 from models.game import Game, GameSchema
 from datetime import date
@@ -10,3 +10,26 @@ def all_games():
     stmt = db.select(Game)
     games = db.session.scalars(stmt)
     return GameSchema(many=True).dump(games)
+
+@games_bp.route('/<int:id>')
+def one_game(id):
+    stmt = db.select(Game).filter_by(id=id)
+    game = db.session.scalar(stmt)
+    if game:
+        return GameSchema().dump(game)
+    else:
+        return {'error': f'Game not found with id {id}'}, 404
+
+@games_bp.route('/', methods = ['POST'])
+def add_game():
+    game = Game(
+        title = request.json['title'],
+        year_released = request.json['year_released'],
+        genre = request.json['genre'],
+        platform = request.json['platform'],
+        date_tracked = date.today(),
+        status = request.json['status']
+    )
+    db.session.add(game)
+    db.session.commit()
+    return GameSchema().dump(game), 201
