ff5528a67735f2d74c9334b296b45137b30aead2
matthewngc <13530@coderacademy.edu.au>
2022-11-12 01:44:43 +1100

    Add auth login controller

--- src/controllers/auth_controller.py
+++ src/controllers/auth_controller.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, request
 from init import db, bcrypt
 from models.user import User, UserSchema
-from datetime import date
+from datetime import date, timedelta
 from sqlalchemy.exc import IntegrityError
+from flask_jwt_extended import create_access_token
 
 auth_bp = Blueprint('auth', __name__, url_prefix = '/auth')
 
@@ -21,9 +22,12 @@ def auth_register():
     except IntegrityError:
         return {'error': 'Email address already in use'}, 409
 
-@auth_bp.route('/login/', method = ['POST'])
+@auth_bp.route('/login/', methods=['POST'])
 def auth_login():
     stmt = db.select(User).filter_by(email=request.json['email'])
     user = db.session.scalar(stmt)
     if user and bcrypt.check_password_hash(user.password, request.json['password']):
-        t
+        token = create_access_token(identity = str(user.id), expires_delta = timedelta(days=1))
+        return {'email': user.email, 'token': token, 'is_admin': user.is_admin}
+    else:
+        return {'error': 'Invalid email or password'}, 401
--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from init import db, ma, bcrypt
+from init import db, ma, bcrypt, jwt
 from controllers.cli_controller import db_commands
 from controllers.games_controller import games_bp
 from controllers.auth_controller import auth_bp
@@ -10,10 +10,12 @@ def create_app():
 
     app.config['JSON_SORT_KEYS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+    app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY')
 
     db.init_app(app)
     ma.init_app(app)
     bcrypt.init_app(app)
+    jwt.init_app(app)
 
     app.register_blueprint(games_bp)
     app.register_blueprint(auth_bp)
